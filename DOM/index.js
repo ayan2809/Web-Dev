af2f563ae900985f2bd623c6c1fa1b59c768ffd3
ayan2809 <ayansadhukhan28@gmail.com>
2021-11-06 23:33:04 +0530

    Jquery+JS+DOM

---
+++ DOM/index.js
@@ -0,0 +1,2 @@
+// to change the h1 tag in that index.html
+document.querySelector("h1").innerHTML="Good Bye";
---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+// const fs= require("fs");
+// fs.copyFileSync("file1.txt","file2.txt");
+// console.log("Hello World");
+var superheroes= require("superheroes"); 
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
